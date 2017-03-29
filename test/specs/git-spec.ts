975ebc9a841d8c47af21ebecab51f852411eb60d
shybyte <ein@volloeko.de>
2017-03-29 09:40:20 +0200

    readBlameStats

--- test/specs/git-spec.ts
+++ test/specs/git-spec.ts
@@ -1,11 +1,20 @@
 import {assert} from 'chai';
-import {readGitStats} from "../../src/client/git";
+import * as mocha from 'mocha';
+import {readBlameStat, readGitStats} from "../../src/client/git";
+
 
 describe('readGitStats', () => {
   it('works', async () => {
     const stats = await readGitStats('../repo-explorer-test-repo');
     assert.isObject(stats.fileStats);
-    console.log(stats.fileStats);
   });
 });
 
+
+
+describe('readBlameStats', () => {
+  it('works', async () => {
+    const stats = await readBlameStat('../repo-explorer-test-repo', 'bower.json');
+    assert.isObject(stats);
+  });
+});
